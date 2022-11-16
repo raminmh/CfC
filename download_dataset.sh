8e5c093cf912ab511c5aca9d748e217937b416bd
Mathias Lechner <mlech26l@gmail.com>
2022-11-16 11:38:19 -0500

    moved dataset to MIT webspace

--- download_dataset.sh
+++ download_dataset.sh
@@ -3,6 +3,7 @@
 mkdir data
 mkdir data/person
 
-wget https://pub.ist.ac.at/~mlechner/datasets/walker.zip
+#wget https://pub.ist.ac.at/~mlechner/datasets/walker.zip
+wget https://people.csail.mit.edu/mlechner/datasets/walker.zip
 unzip walker.zip -d data/
 rm walker.zip
