bc84d6ca79f3baf3da7181dd3d7a1ae1ed5b6b6e
Daniel Scott <6148219+nightvision04@users.noreply.github.com>
2022-12-13 19:31:14 -0700

    Added pypi description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    ]
+
 setup(
     name='cfc_model',
-    version='1.0.8',    
+    version='1.0.9',    
     description='An easy-to-use api for the closed-form continuous models in tensorflow',
     url='https://github.com/nightvision04/CfC',
     author='Daniel Scott',
@@ -11,6 +15,8 @@ setup(
     install_requires=['pandas',
                       'numpy',
                       ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     
     packages=setuptools.find_packages(),
     include_package_data=True,
